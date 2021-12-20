e15e66d2e1d1265878120fb9d8eda999a0a75c0a
Felipekod <55560328+Felipekod@users.noreply.github.com>
2021-12-20 18:07:55 -0500

    first commit

---
+++ ppmtool/src/test/java/ca/com/felipeoliveira/ppmtool/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.com.felipeoliveira.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
